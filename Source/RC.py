d0cfa4d819b54d6711ee567cde6b975f105f7079
Hengyang Zhao <hzhao@ece.ucr.edu>
2016-08-13 19:13:10 -0700

    Add timer in status bar

--- Source/RC.py
+++ Source/RC.py
@@ -18,7 +18,7 @@ def cursesColor(colorStr):
     }
     return curses.color_pair(colorDict[colorStr])
 
-class Cell:
+class StyledText:
 
     def __init__(self, text, style=None, color=None):
         self._text = text
@@ -31,22 +31,22 @@ class Cell:
     def attr(self):
         return self._style | cursesColor(self._color)
 
-CELL_DIGIT_NONE = Cell('   ', 0)
-CELL_DIGIT_1    = Cell(' 1 ', curses.A_BOLD, 'blue')
-CELL_DIGIT_2    = Cell(' 2 ', 0, 'green')
-CELL_DIGIT_3    = Cell(' 3 ', curses.A_BOLD, 'red')
-CELL_DIGIT_4    = Cell(' 4 ', curses.A_BOLD, 'magenta')
-CELL_DIGIT_5    = Cell(' 5 ', 0, 'red')
-CELL_DIGIT_6    = Cell(' 6 ', 0, 'cyan')
-CELL_DIGIT_7    = Cell(' 7 ', 0, 'yellow')
-CELL_DIGIT_8    = Cell(' 8 ', curses.A_BOLD, 'white')
+CELL_DIGIT_NONE = StyledText('   ', 0)
+CELL_DIGIT_1    = StyledText(' 1 ', curses.A_BOLD, 'blue')
+CELL_DIGIT_2    = StyledText(' 2 ', 0, 'green')
+CELL_DIGIT_3    = StyledText(' 3 ', curses.A_BOLD, 'red')
+CELL_DIGIT_4    = StyledText(' 4 ', curses.A_BOLD, 'magenta')
+CELL_DIGIT_5    = StyledText(' 5 ', 0, 'red')
+CELL_DIGIT_6    = StyledText(' 6 ', 0, 'cyan')
+CELL_DIGIT_7    = StyledText(' 7 ', 0, 'yellow')
+CELL_DIGIT_8    = StyledText(' 8 ', curses.A_BOLD, 'white')
 
-CELL_FLAGGED    = Cell(' $ ', curses.A_REVERSE | curses.A_BOLD, 'white')
-CELL_UNEXPLORED = Cell('   ', curses.A_REVERSE | curses.A_DIM)
+CELL_FLAGGED    = StyledText(' $ ', curses.A_REVERSE | curses.A_BOLD, 'white')
+CELL_UNEXPLORED = StyledText('   ', curses.A_REVERSE | curses.A_DIM)
 
-CELL_WRONG          = Cell('!X!', curses.A_REVERSE | curses.A_BOLD, 'yellow')
-CELL_UNFLAGGED_MINE = Cell(' * ', curses.A_REVERSE)
-CELL_BOOMED         = Cell('BM!', curses.A_REVERSE | curses.A_BOLD, 'red')
+CELL_WRONG          = StyledText('!X!', curses.A_REVERSE | curses.A_BOLD, 'yellow')
+CELL_UNFLAGGED_MINE = StyledText(' * ', curses.A_REVERSE)
+CELL_BOOMED         = StyledText('BM!', curses.A_REVERSE | curses.A_BOLD, 'red')
 
 MINE_FIELD_CELL_CHEIGHT = 1
 MINE_FIELD_CELL_CWIDTH  = 3
@@ -61,6 +61,12 @@ MINE_FIELD_HEIGHT = 16
 MINE_FIELD_WIDTH = 30
 MINE_FIELD_MINES = 99
 MINE_FIELD_DEFAULT_MINES_PERCENTAGE = 0.16
+
+BUTTON_NEW_GAME = StyledText(' New Game ', curses.A_REVERSE, 'white')
+BUTTON_PAUSE    = StyledText('  PAUSE  ', curses.A_REVERSE, 'white')
+BUTTON_RECORDS  = StyledText(' RECORDS ', curses.A_REVERSE, 'white')
+
+TIMER = StyledText(' %s ', curses.A_REVERSE, 'white')
 
 SIDE_PANE_WIDTH = 32
 SHELL_COMMAND_STYLE = 0
--- Source/Termine.py
+++ Source/Termine.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 import curses
+import datetime
 from collections import deque
+from collections import OrderedDict
 
 import RC
 import Shell
+
+MINE_FIELD_WINDOW = None
+STATUS_WINDOW = None
+LOG_WINDOW = None
+SHELL = None
 
 class MineFieldWindow:
 
     def __init__(self, screen, width, shell):
 
-        self._win = curses.newwin(curses.LINES, width, 0, 0)
+        self._win = curses.newwin(curses.LINES - 1, width, 0, 0)
         self._shell = shell
 
     def getMaxMineFieldSize(self):
@@ -26,7 +33,7 @@ class MineFieldWindow:
         self._win.erase()
         self._win.border()
         self.redrawMineField()
-        self._win.refresh()
+        self._win.noutrefresh()
 
     def retrieveMineFieldCoordinate(self, cX, cY):
 
@@ -153,7 +160,7 @@ class MineFieldWindow:
         elif cell == 'mine':
             return RC.CELL_UNFLAGGED_MINE
         else:
-            raise Exception("Unknown cell type: %s" % cell) 
+            raise Exception("Unknown cell type: %s" % cell)
 
     def _mineFieldXYCStart(self):
 
@@ -175,11 +182,91 @@ class MineFieldWindow:
 
         return xCEnd, yCEnd
 
+class StatusWindow:
+
+    BUTTON_NEW_GAME = 0
+    BUTTON_PAUSE = 1
+    BUTTON_RECORDS = 2
+
+    def __init__(self, screen, width, shell):
+        self._win = curses.newwin(1, width, curses.LINES - 1, 0)
+        self._startTime = None
+        self._elapsedTime = None
+        self._buttons = OrderedDict({
+                StatusWindow.BUTTON_NEW_GAME: RC.BUTTON_NEW_GAME,
+                StatusWindow.BUTTON_PAUSE: RC.BUTTON_PAUSE,
+                StatusWindow.BUTTON_RECORDS: RC.BUTTON_RECORDS
+                })
+
+    def isTimerReset(self):
+        return self._startTime is None and self._elapsedTime is None
+
+    def startTimer(self):
+        self._startTime = datetime.datetime.now()
+        self._elapsedTime = None
+
+    def resetTimer(self):
+        self._startTime = None
+        self._elapsedTime = None
+
+    def stopTimer(self):
+        assert self._startTime is not None
+        if self._elapsedTime is None:
+            self._elapsedTime = datetime.datetime.now() - self._startTime
+
+    def clockString(self):
+        if self._startTime is None:
+            return "00:00.0"
+
+        elapsed = self._elapsedTime if self._elapsedTime is not None else datetime.datetime.now() - self._startTime
+
+        nmins = elapsed.seconds // 60
+        nsecs = elapsed.seconds % 60
+        ntenth = elapsed.microseconds // 100000
+
+        if (nmins > 99):
+            nmins = 99
+            nsecs = 99
+            ntenth = 9
+
+        return "%02d:%02d.%01d" % (nmins, nsecs, ntenth)
+
+    def drawClock(self):
+        timeStr = RC.TIMER.text() % self.clockString()
+        _, cWidth = self._win.getmaxyx()
+
+        self._win.addstr(0, cWidth - len(timeStr) - 1, timeStr, RC.TIMER.attr())
+
+    def drawButtons(self):
+        cStart = 0
+
+        for btn in self._buttons.values():
+            self._win.addstr(0, cStart, btn.text(), btn.attr())
+            cStart += 1 + len(btn.text())
+
+    def retrieveClickedButton(self, cX, cY):
+        if cY != curses.LINES - 1:
+            return None
+
+        cStart = 0
+        for k, btn in self._buttons.items():
+
+            if cX >= cStart and cX < cStart + len(btn.text()):
+                return k
+            cStart += 1 + len(btn.text())
+
+        return None
+
+    def updateAll(self):
+        self.drawButtons()
+        self.drawClock()
+        self._win.noutrefresh()
+
 class LogWindow:
 
     def __init__(self, screen, width, shell):
 
-        self._win = curses.newwin(curses.LINES - 1, width, 0, curses.COLS - width)
+        self._win = curses.newwin(curses.LINES, width, 0, curses.COLS - width)
         self._shell = shell
         self._logLines = deque()
 
@@ -199,21 +286,15 @@ class LogWindow:
             self._win.addstr(y + 1, x + 1, line)
             y += 1
 
-        self._win.border()
-
     def updateAll(self):
 
         self.printLog()
-
-        self._win.refresh()
+        self._win.border()
+        self._win.noutrefresh()
 
     def _maxLines(self):
         cHeight, _ = self._win.getmaxyx()
         return cHeight - RC.LOG_BORDER_CHEIGHT * 2
-
-MINE_FIELD_WINDOW = None
-LOG_WINDOW = None
-SHELL = None
 
 class EventLoop:
 
@@ -223,17 +304,24 @@ class EventLoop:
     def mineFieldOnMouseClick(self, mX, mY, btn):
 
         coor = MINE_FIELD_WINDOW.retrieveMineFieldCoordinate(mX, mY)
-        LOG_WINDOW.push("%d %d %d" % (mX, mY, btn))
 
         if coor is None:
             return
 
         if btn == curses.BUTTON1_PRESSED:
+
+            if STATUS_WINDOW.isTimerReset():
+                STATUS_WINDOW.startTimer()
+
             x, y = coor
             LOG_WINDOW.push("poke %d %d" % (x, y))
             SHELL.run("poke %d %d" % (x, y))
             for line in SHELL.getOutput():
                 LOG_WINDOW.push(line)
+
+            if MINE_FIELD_WINDOW.isBoomed():
+                STATUS_WINDOW.stopTimer()
+
         elif btn == curses.BUTTON3_PRESSED:
             x, y = coor
             LOG_WINDOW.push("toggle %d %d" % (x, y))
@@ -243,19 +331,28 @@ class EventLoop:
         else:
             pass
 
-    def newGameButtonOnMouseClick(self, mX, mY, btn):
+    def statusBarOnMouseClick(self, mX, mY):
+
+        button = STATUS_WINDOW.retrieveClickedButton(mX, mY)
+        if button == StatusWindow.BUTTON_NEW_GAME:
+            self.newGameButtonOnMouseClick()
+        elif button == StatusWindow.BUTTON_PAUSE:
+            self.pauseGameButtonOnMouseClick()
+        elif button == StatusWindow.BUTTON_RECORDS:
+            self.bestRecordButtonOnMouseClick()
+        else: pass
+
+    def newGameButtonOnMouseClick(self):
+        LOG_WINDOW.push("OMG")
+        RestartGame()
+
+    def pauseGameButtonOnMouseClick(self):
         pass
 
-    def pauseGameButtonOnMouseClick(self, mX, mY, btn):
-        pass
-
-    def bestRecordButtonOnMouseClick(self, mX, mY, btn):
+    def bestRecordButtonOnMouseClick(self):
         pass
 
     def run(self):
-
-        global MINE_FIELD_WINDOW
-        global LOG_WINDOW
 
         while True:
 
@@ -266,10 +363,16 @@ class EventLoop:
                 _, mX, mY, _, btn = mouseEvent
 
                 self.mineFieldOnMouseClick(mX, mY, btn)
+                self.statusBarOnMouseClick(mX, mY)
 
-            MINE_FIELD_WINDOW.updateAll()
-            LOG_WINDOW.updateAll()
-            curses.doupdate()
+                refreshAll()
+
+def refreshAll():
+
+    MINE_FIELD_WINDOW.updateAll()
+    STATUS_WINDOW.updateAll()
+    LOG_WINDOW.updateAll()
+    curses.doupdate()
 
 def InitColor():
     curses.init_pair(0, curses.COLOR_BLACK, -1)
@@ -292,26 +395,35 @@ def InitCurses():
     curses.mouseinterval(0)
     curses.curs_set(False)
 
+def RestartGame():
+
+    mfWidthMax, mfHeightMax = MINE_FIELD_WINDOW.getMaxMineFieldSize()
+    if mfWidthMax >= RC.MINE_FIELD_WIDTH and mfHeightMax >= RC.MINE_FIELD_HEIGHT:
+        shellCmd = "minefield %d %d %d" % (RC.MINE_FIELD_WIDTH, RC.MINE_FIELD_HEIGHT, RC.MINE_FIELD_MINES)
+    else:
+        shellCmd = "minefield %d %d %d" % (mfWidthMax, mfHeightMax, int(mfWidthMax * mfHeightMax * RC.MINE_FIELD_DEFAULT_MINES_PERCENTAGE))
+    SHELL.run(shellCmd)
+    STATUS_WINDOW.resetTimer()
+
 def Main(stdscr):
 
     global MINE_FIELD_WINDOW
+    global STATUS_WINDOW
     global LOG_WINDOW
     global SHELL
 
     InitCurses()
-
     stdscr.clear()
 
     SHELL = Shell.Shell()
     MINE_FIELD_WINDOW = MineFieldWindow(stdscr, curses.COLS - RC.LOG_WINDOW_WIDTH, SHELL)
+    STATUS_WINDOW = StatusWindow(stdscr, curses.COLS - RC.LOG_WINDOW_WIDTH, SHELL)
     LOG_WINDOW = LogWindow(stdscr, RC.LOG_WINDOW_WIDTH, SHELL)
 
-    mfWidthMax, mfHeightMax = MINE_FIELD_WINDOW.getMaxMineFieldSize()
-    if mfWidthMax >= RC.MINE_FIELD_WIDTH and mfHeightMax >= RC.MINE_FIELD_HEIGHT:
-        SHELL.run("minefield %d %d %d" % (RC.MINE_FIELD_WIDTH, RC.MINE_FIELD_HEIGHT, RC.MINE_FIELD_MINES))
-    else:
-        SHELL.run("minefield %d %d %d" % (mfWidthMax, mfHeightMax, int(mfWidthMax * mfHeightMax * RC.MINE_FIELD_DEFAULT_MINES_PERCENTAGE)))
+    RestartGame()
 
+    stdscr.refresh()
+    refreshAll()
     EventLoop(stdscr).run()
 
 curses.wrapper(Main)
